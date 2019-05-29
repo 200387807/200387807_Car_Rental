ed1c741a47f7d43dbec68b1e7516e45db046d169
200387807 <46694599+200387807@users.noreply.github.com>
2019-05-29 17:01:19 -0400

    Add project files.

---
+++ 200387807_Car_Rental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_200387807_Car_Rental.Startup))]
+namespace _200387807_Car_Rental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
